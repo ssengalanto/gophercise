fda6933cfa32cade0742a760c60dafd60709300e
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-01-14 10:03:35 +0800

    leet code: fizz buzz

---
+++ 014_fizz-buzz/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(fizzBuzz(15))
+}
+
+func fizzBuzz(n int) []string {
+	res := make([]string, 0, n)
+	for i := 1; i <= n; i++ {
+		var s string
+		if i%3 == 0 {
+			s += "Fizz"
+		}
+		if i%5 == 0 {
+			s += "Buzz"
+		}
+		if s == "" {
+			s = strconv.Itoa(i)
+		}
+		res = append(res, s)
+	}
+	return res
+}
