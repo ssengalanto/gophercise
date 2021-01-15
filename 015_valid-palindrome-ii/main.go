1e5b1f6bcb35ce10b21ebd1763f4c59e0c3a54c8
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-01-15 09:53:48 +0800

    leet code: is valid palindrome 2

---
+++ 015_valid-palindrome-ii/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(validPalindrome("acbba"))
+}
+
+func validPalindrome(s string) bool {
+	i, j := 0, len(s)-1
+	for i < j {
+		if s[i] != s[j] {
+			return isPalindrome(s[i+1:j+1]) || isPalindrome(s[i:j])
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isPalindrome(s string) bool {
+	fmt.Println(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
