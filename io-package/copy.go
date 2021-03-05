6ce855a041e75bc843861b0b74159cfe5543307d
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-03-05 12:04:25 +0800

    io: copy

---
+++ io-package/copy.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	s := "some io.Reader stream to be read\n"
+	r := strings.NewReader(s)
+
+	if a, err := io.Copy(os.Stdout, r); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(a, len(s)
+	}
+}
