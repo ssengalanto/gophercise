a6959d74fe277b4171bb6a5a136a28e0057a8ca5
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-03-09 10:20:45 +0800

    path: split

---
+++ path/split.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"path"
+)
+
+func main() {
+	split := func(s string) {
+		dir, file := path.Split(s)
+		fmt.Printf("path.Split(%q) = dir: %q, file: %q\n", s, dir, file)
+	}
+	split("static/myfile.css")
+	split("dev/static/myfile.css")
+	split("myfile.css")
+	split("")
+}
