f6b8cf31357864c61b70099b653682d6e53f7d8e
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-03-09 10:01:02 +0800

    path: ext

---
+++ path/ext.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"path"
+)
+
+func main() {
+	fmt.Println(path.Ext("/a/b/c/bar.css"))
+	fmt.Println(path.Ext("/test.go"))
+	fmt.Println(path.Ext("/"))
+	fmt.Println(path.Ext(""))
+}
